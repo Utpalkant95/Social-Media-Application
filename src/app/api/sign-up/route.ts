e2d3f122fe93a4ceeccd775fbc60a5cc05143f92
Utpalkant <127578526+Utpalkant95@users.noreply.github.com>
2024-08-27 13:36:30 +0530

    phone otp send

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,8 +1,9 @@
-import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/model/User";
-import bcryptjs from "bcryptjs";
-import { redirect } from "next/navigation";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { sendPhoneOtp } from "@/helpers/sendVerificationSms";
+import dbConnect from "@/lib/dbConnect";
+import UserModel, { User } from "@/model/User";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import bcryptjs from "bcryptjs";
 
 /**
  *
@@ -52,67 +53,141 @@ export async function POST(request: Request) {
   try {
     const { fullName, userName, email, phone, password } = await request.json();
 
-    if (!fullName || !userName || !email || !phone || !password) {
-      return Response.json(
-        {
-          message: "All fields are required",
-        },
-        {
-          status: 400,
-        }
-      );
+    // validation
+    // Validate input fields
+    const validation = signUpSchema.safeParse({
+      fullName,
+      userName,
+      email,
+      phone,
+      password,
+    });
+
+    if (!validation.success) {
+      return Response.json({
+        success : false,
+        message : validation.error.errors[0].message,
+        data : validation
+      }, {
+        status : 400
+      })
     }
 
+    //  checking existing user by email or username
     const existingUser = await UserModel.findOne({
       $or: [{ userName: userName }, { email: email }],
     });
 
     if (existingUser) {
-      return Response.json({
-        success: false,
-        message: "User Is Already Exist. Please Login",
-      }, {
-        status : 409
-      });
-    } 
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "User already exists",
+        }),
+        {
+          status: 400,
+        }
+      )
+    }
 
+    // checking user validation
+    // if (existingUser && !existingUser.emailVerified && !existingUser.phoneVerified) {
+    //   return Response.json({
+    //     success : false,
+    //     message : "User is not verified",
+        
+    //   })
+    // }
+
+    // execution
+
+    // generating phone and email verification opts
+    const phoneOtp = Math.floor(100000 + Math.random() * 900000);
+    const emailOtp = Math.floor(100000 + Math.random() * 900000);
+    
+    // hasging the password 
     const hashedPassword = await bcryptjs.hash(password, 10);
 
-    const newUser = await new UserModel({
-      fullName: fullName,
-      userName: userName,
-      email: email,
-      phone: phone,
+    // making new user 
+    const newUser = new UserModel({
+      fullName,
+      userName,
+      email,
+      phone,
+      phoneOtp : phoneOtp,
+      emailOtp : emailOtp,
       password: hashedPassword,
       isVerified: false,
-      isFirstTimeLogin: false,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
+      isFirstTimeLogin: true
     });
 
     const savedUser = await newUser.save();
 
     if (!savedUser) {
-      return Response.json({
-        success: false,
-        message: "Something went wrong. Please try again",
-      });
-    } else {
-      return Response.json({
-        success: true,
-        message: "User Created Successfully",
-      });
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "Something went wrong. Please try again.",
+        }),
+        { status: 500 }
+      );
     }
+
+    // // send verification email
+    // const emailResponse = await sendVerificationEmail({
+    //   email : email,
+    //   emailOtp : emailOtp,
+    //   firstName : fullName,
+    //   phoneOtp : phoneOtp
+    // })
+
+    // console.log("emailResponse", emailResponse);
+    
+
+    // if (!emailResponse) {
+    //   return new Response(
+    //     JSON.stringify({
+    //       success: false,
+    //       message: "something went wrong while sending email. Please try again.",
+    //     }),
+    //     { status: 500 }
+    //   );
+    // }
+    // const phoneNo = "+91" + phone;
+    // console.log("phoneNo", phoneNo);
+    
+    // const smsResponse =  await sendPhoneOtp(phoneNo, String(phoneOtp))
+
+    // console.log("smsResponse", smsResponse);
+
+    // if (!smsResponse) {
+    //   return new Response(
+    //     JSON.stringify({
+    //       success: false,
+    //       message: "something went wrong while sending sms. Please try again.",
+    //     }),
+    //     { status: 500 }
+    //   );
+    // }
+
+    // response
+    
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "User created successfully",
+      }),
+      { status: 200 }
+    );
+
   } catch (error) {
-    console.log("error while registering user", error);
-    return Response.json(
-      {
+    console.log("Error while registering user:", error);
+    return new Response(
+      JSON.stringify({
         success: false,
-        message: "Erorr in Registering User",
-      },
-      {
-        status: 500,
-      }
+        message: "Error in registering user",
+      }),
+      { status: 500 }
     );
   }
 }
