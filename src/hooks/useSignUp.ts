c1b4c2b071fbc14f750de65959198de348f4aef5
Utpalkant <127578526+Utpalkant95@users.noreply.github.com>
2024-08-27 13:07:59 +0530

    user signup done

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,93 +1,24 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
-import { useForm } from "react-hook-form";
 import { z } from "zod";
-import axios from "axios";
-import { enqueueSnackbar } from "notistack";
 import { useRouter } from "next/navigation";
-
-const formSchema = z.object({
-  email: z
-    .string()
-    .email("Please enter a valid email address.")
-    .regex(
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Invalid email address"
-    )
-    .trim(),
-  fullName: z
-    .string()
-    .min(5, "Full Name must be at least 5 characters.")
-    .trim()
-    .regex(
-      /^[A-Za-z ]+$/,
-      "Full Name must not contain numbers or special characters"
-    ),
-  userName: z
-    .string()
-    .min(3, "Username must be at least 3 characters.")
-    .trim()
-    .regex(
-      /^[a-z0-9]+$/,
-      "Username must contain only lower letters and numbers."
-    ),
-  phone: z
-    .string()
-    .min(10, "Phone Number length must be 10.")
-    .regex(
-      /^[6-9]\d{9}$/,
-      "Phone must start with 6, 7, 8, or 9 and be exactly 10 digits."
-    )
-    .regex(
-      /^\d{10}$/,
-      "Phone must contain exactly 10 digits and no letters or special characters."
-    ),
-  password: z.string().min(2, "Password must be at least 2 characters."),
-});
+import { signUpSchema } from "@/schemas/signUpSchema";
+import { signUpUser } from "@/ApiServices/AuthServices";
+import { useSignUpForm } from "@/forms";
 
 const useSignUp = () => {
   const router = useRouter();
-  const signUpFun = async (data: any): Promise<unknown> => {
-    const response = await axios.post(
-      "http://localhost:3000/api/sign-up",
-      data
-    );
-    const user = await response.data;
-    return user;
-  };
-  const {
-    mutate,
-    data: signUpData,
-    isLoading,
-    error,
-    isSuccess,
-    status,
-  } = useMutation({
+  const form = useSignUpForm();
+
+  const { data, mutate, isLoading, status } = useMutation({
     mutationKey: ["signUp"],
-    mutationFn: (data: any) => signUpFun(data),
-    onSuccess: (signUpData: any) => {
-      enqueueSnackbar(signUpData && (signUpData.message as string), {
-        variant: "success",
-      });
-      router.push("/account/sign-in");
-    },
-  });
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      fullName: "",
-      userName: "",
-      phone: "",
-      password: "",
-    },
+    mutationFn:signUpUser,
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = (values: z.infer<typeof signUpSchema>) => {
     mutate(values);
   };
-
-  return { form, onSubmit, signUpData, isLoading, error, isSuccess, status };
+  return { form, onSubmit, status, isLoading, data };
 };
 
 export default useSignUp;
+
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -3,6 +3,7 @@ import {z} from "zod";
 export const signUpSchema = z.object({
     fullName: z.string().min(5, "Full Name must be at least 5 characters long").regex( /[^a-zA-Z\s]/ , "Full Name must not contain numbers or special characters"),
     email: z.string().email("Invalid email address").regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email address"),
+    phone : z.string().min(10, "Phone must be at least 10 characters long").regex(/^[0-9]+$/, "Phone must contain only numbers"),
     userName : z.string().min(5, "User Name must be at least 5 characters long").regex( /[^a-zA-Z0-9\s]/ , "User Name must not contain special characters"),
     password : z.string().min(8, "Password must be at least 8 characters long"),
 })
