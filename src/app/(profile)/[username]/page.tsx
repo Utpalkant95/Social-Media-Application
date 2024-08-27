fb7a17403f34f70f8ac354ab43fd0e43969484cf
Utpalkant <127578526+Utpalkant95@users.noreply.github.com>
2024-08-27 12:41:53 +0530

    user profile page me real data render kra

--- src/app/(profile)/[username]/page.tsx
+++ src/app/(profile)/[username]/page.tsx
@@ -1,11 +1,26 @@
-import { ProfileFrag } from "@/Fragments"
-const Page = ({params} : {params: {username: string}}) => {
-    const {username} = params
-  return (
-    <ProfileFrag>
-      {username}
-    </ProfileFrag>
-  )
-}
+"use client";
+import { getSignleUserData } from "@/ApiServices/UserServices";
+import { ProfileFrag } from "@/Fragments";
+import { decodeToken } from "@/helpers/userInfo";
+import { User } from "@/model/User";
+import { useMutation, useQuery } from "@tanstack/react-query";
+const Page = ({ params }: { params: { username: string } }) => {
+  const { username } = params;
+  const user = decodeToken();
+  console.log(user?.userId);
 
-export default Page
+ const {data} = useQuery({
+   queryKey: ["user"],
+   queryFn: () => getSignleUserData(username),
+ })
+
+ const userData : User = data?.data
+  
+  const ownViewer= user?.username === username;
+  
+  // fetch user details
+
+  return <ProfileFrag user = {userData} ownViewer = {ownViewer}>{username}</ProfileFrag>;
+};
+
+export default Page;
