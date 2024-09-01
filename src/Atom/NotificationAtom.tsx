f3e86412bcacf6dc39c77cb5bc155e3e058be894
Utpalkant <127578526+Utpalkant95@users.noreply.github.com>
2024-09-01 21:05:42 +0530

    repush

--- src/Atom/NotificationAtom.tsx
+++ src/Atom/NotificationAtom.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 
 const NotificationAtom = () => {
-  console.log("notification");
-  const [count, setCount] = useState<number>(0)
   return (
     <div>NotificationAtom</div>
   )
