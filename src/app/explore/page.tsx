c3888052795ce376e665c694e0f24d7d5c9761d7
Utpal Kant <utpalkant881@gmail.com>
2024-11-01 16:11:27 +0530

    coooend adding

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
     queryKey: ["explore"],
     queryFn: explorePosts,
   });
-
+// lets check the code feuirrni
   return (
     <div className="max-w-6xl mx-auto w-full pt-2 sm:pt-8">
       {isLoading && (
